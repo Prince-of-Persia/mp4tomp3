5ca724b987379ee400e295a7bc516c091e27c7a4
Amir <amir_rajabifar@yahoo.com.au>
2016-10-17 20:47:19 +0800

    Fix script, This is the end of this feature

--- mp4tomp3.sh
+++ mp4tomp3.sh
@@ -1,9 +1,11 @@
-cp -a /$2/. /$2/  #copy the whole dir from src to dest
+cp -a /$1/. /$2/  #copy the whole dir from src to dest
 
 #cd $2 #change dir to dest
-
+cd $2
 #convert *.mp4 to *.mp3
-find $2 -name "*.mp4" -exec bash -c 'var=${1%.mp4}; var=${var#*/};ffmpeg -i "${1#*/}" -vn -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000 "$2/$var".mp3' - '{}' \;
+#find . -name "*.mp4" -exec bash -c 'var=${1%.mp4}; var=${var#*/};ffmpeg -i "${1#*/}" -vn -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000 "$dest/$var".mp3' - '{}' \;
 
-#echo "Cleaning up" 
-#find $2 -name "*.mp4" -exec bash -c 'var=${1%.mp4}; var=${var#*/}; rm "${1#*/}"' - '{}' \;
+find . -name "*.mp4" -exec bash -c 'ffmpeg -i "$1" -vn -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000 "${1%.mp4}".mp3' - '{}' \;
+
+echo "Cleaning up" 
+find . -name "*.mp4" -exec bash -c 'var=${1%.mp4}; var=${var#*/}; rm "${1#*/}"' - '{}' \;
